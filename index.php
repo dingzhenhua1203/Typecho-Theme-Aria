574a2302f5b21f76ca3a602ef7ff9e6f3b5ab3ce
dingzhenhua <zhenhua.ding@ly.com>
2020-03-16 13:32:18 +0800

    美化aria主题

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 书写属于自己的篇章
  * 
@@ -11,46 +12,45 @@
  */
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
- $this->need('header.php');
- ?>
+$this->need('header.php');
+?>
 
-<div id="main" class="col-mb-12 col-8 col-offset-1" >
-	<?php while($this->next()): ?>
-            <article itemscope itemtype="http://schema.org/BlogPosting" class="card animated wow fadeIn" data-wow-duration="1s" data-wow-offset="10" style='padding:10px ;margin:40px auto;'>
-                <a href="<?php $this->permalink(); ?>">
-                <div >
+<div id="main" class="col-mb-12 col-8 col-offset-1">
+    <?php while ($this->next()) : ?>
+        <article itemscope itemtype="http://schema.org/BlogPosting" class="card animated wow fadeIn" data-wow-duration="1s" data-wow-offset="10" style='padding:0px ;margin:40px auto;'>
+            <a href="<?php $this->permalink(); ?>">
+                <div>
                     <?php $this->sticky(); ?>
-                </div>
-                    <?php if(Utils::isEnabled('enableLazyload','AriaConfig')): ?>
-                    <div class="card-thumbnail lazyload" data-original=<?php if($this->fields->thumbnail)
-                                $this->fields->thumbnail();
-                            else
-                                echo Utils::getThumbnail();
-                        ?> style="background:url(<?php $this->options->themeUrl('assets/img/loading.svg') ?>) center center no-repeat;background-size: 100% auto;">
-                    </div>
-                    <?php else: ?>
-                    <div class="card-thumbnail" style="background:url(<?php if($this->fields->thumbnail)
-                                $this->fields->thumbnail();
-                            else
-                                echo Utils::getThumbnail();
-                        ?>) center center no-repeat;background-size: 100% auto;">
-                    </div>
+                    <?php if (Utils::isEnabled('enableLazyload', 'AriaConfig')) : ?>
+                        <div class="card-thumbnail lazyload" data-original=<?php if ($this->fields->thumbnail)
+                                                                                $this->fields->thumbnail();
+                                                                            else
+                                                                                echo Utils::getThumbnail();
+                                                                            ?> style="background:url(<?php $this->options->themeUrl('assets/img/loading.svg') ?>) center center no-repeat;background-size: 100% auto;">
+                        </div>
+                    <?php else : ?>
+                        <div class="card-thumbnail" style="background:url(<?php if ($this->fields->thumbnail)
+                                                                                $this->fields->thumbnail();
+                                                                            else
+                                                                                echo Utils::getThumbnail();
+                                                                            ?>) center center no-repeat;background-size: 100% auto;">
+                        </div>
                     <?php endif; ?>
                     <div class="articalSite">
                         <a class='articalTitle' href="<?php $this->permalink(); ?>"><?php $this->title(); ?></a>
                         <div class='articalDescription'>
-                                <span> <?php $this->date(); ?>・</span><span><?php  $this->category('・',false,'无');   ?>・</span>
-                                <span ><i class="iconfont icon-aria-comment"></i> <?php $this->commentsNum('%d'); ?> </span>
-                        </div> 
+                            <span> <?php $this->date(); ?>・</span><span><?php $this->category('・', false, '无');   ?>・</span>
+                            <span><i class="iconfont icon-aria-comment"></i> <?php $this->commentsNum('%d'); ?> </span>
+                        </div>
                     </div>
-                </a>
-            </article>
-	<?php endwhile; ?>
+                </div>
+            </a>
+        </article>
+    <?php endwhile; ?>
 
-     <div id="page-nav">
-        <?php $this->pageNav('<', '>',1,'...',array('wrapTag' => 'ul', 'wrapClass' => '','itemTag' => 'li','currentClass' => 'page-current',)); ?>  
-     </div>
+    <div id="page-nav">
+        <?php $this->pageNav('<', '>', 1, '...', array('wrapTag' => 'ul', 'wrapClass' => '', 'itemTag' => 'li', 'currentClass' => 'page-current',)); ?>
+    </div>
 </div><!-- end #main-->
 <?php $this->need('sidebar.php'); ?>
 <?php $this->need('footer.php'); ?>
-
